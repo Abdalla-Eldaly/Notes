adcf9d97c11c810628aec11fe0866bd7c823dc65
Abdalla El-daly <abdalla90eldaly90@gmail.com>
2024-03-01 17:32:13 +0200

    change theme color and build NoteView

--- lib/view/screens/noteView.dart
+++ lib/view/screens/noteView.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/customAppbar.dart';
+
 class NoteView extends StatelessWidget {
   const NoteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return   const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          children: [
+            SizedBox(height: 60,),
+            CustomAppbar()
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
